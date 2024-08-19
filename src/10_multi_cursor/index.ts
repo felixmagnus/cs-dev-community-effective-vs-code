f4b95ce4ec3ef8f625fdb99a83ce23617108d07e
Felix Magnus <felix.magnus@codecentric.de>
2024-08-19 10:13:45 +0200

    add one more exercise for multicursor

--- src/10_multi_cursor/index.ts
+++ src/10_multi_cursor/index.ts
@@ -21,7 +21,8 @@ const x = () => {
 //    - Repeat until you have a cursor on each name
 //    - Using Shift + Option + Arrow-Right, select the name
 //    - Press Cmd + C to copy the names
-//    - Move your cursor to the start of the line
+//
+//    - Move your cursors to the start of the line
 //    - Press Shift + Option + Arrow-Right to select the respective "target"
 //    - Press Cmd + V to paste the names
 
@@ -32,3 +33,24 @@ const greetings = {
   target4: "Hello, anna!",
   target5: "Hello, franzi!",
 };
+
+// C) - Put your cursor on the first "const" keyword
+//    - Press Cmd + D to select the next "const" keyword
+//    - Repeat until all three "const" keywords are selected
+//    - Select and copy all three variable names
+//
+//    - Discard your multi cursors by pressing Esc
+//    - Select the first "console.log" statement
+//    - Press Cmd + D repeatedly to select all following "console.log" statements
+//    - Move your cursor and select the "Log X" strings
+//    - Paste the variable names
+
+const someVariable = "hello";
+
+const thisVar = "bla";
+
+const thatVar = "smisma";
+
+console.log("Log 1");
+console.log("Log 2");
+console.log("Log 3");
